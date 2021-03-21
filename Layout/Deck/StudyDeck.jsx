92fd953b89bee8e886f6abc4cae1712c4ad90a0e
Austin Mckee <captainlantern8@gmail.com>
2021-03-21 11:12:16 -0700

    study done

--- Layout/Deck/StudyDeck.jsx
+++ Layout/Deck/StudyDeck.jsx
@@ -3,10 +3,29 @@ import BreadCrumb from "../BreadCrumb";
 import StudyCard from "../Card/StudyCard";
 
 export default function StudyDeck({ decks, deck }) {
+  let cardAmount = 0;
+  if (deck.cards != undefined) {
+    cardAmount = deck.cards.length;
+  } else {
+    cardAmount = 0;
+  }
+
   return (
     <div>
       <BreadCrumb decks={decks} />
-      <StudyCard deck={deck} />
+      <h2> {deck.name}: Study</h2>
+
+      {cardAmount > 2 ? (
+        <StudyCard deck={deck} />
+      ) : (
+        <div>
+          <h3>Not Enough Cards.</h3>
+          <p>
+            You need at least 3 cards to study. There are {cardAmount} cards in
+            this deck
+          </p>
+        </div>
+      )}
     </div>
   );
 }
